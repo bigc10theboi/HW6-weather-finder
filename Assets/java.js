6b8e9c17bb675ef346e912e43621d98881035fa4
Carter Venne <you@example.com>
2021-01-26 18:13:52 -0600

    added in variables

---
+++ Assets/java.js
@@ -0,0 +1,4 @@
+var key = '4396c33210ea436c01b4d4cc2a212e09';
+var city = $('#cityName').val().trim();
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=4396c33210ea436c01b4d4cc2a212e09";
+
